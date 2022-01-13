3bfb134ec98cf59e41252ce02af64aaad80ec926
fanhaoyuan <418536538@qq.com>
2022-01-13 20:25:58 +0800

    feat: 添加独立应用

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ExceptionsFilter } from './filters';
+import { GiteeActionsModule } from './gitee-actions';
+
+@Module({
+    imports: [GiteeActionsModule.register()],
+})
+class AppModule {}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalFilters(new ExceptionsFilter());
+    await app.listen(3000);
+}
+bootstrap();
