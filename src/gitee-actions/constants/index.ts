a637ea47e5d1942cda6097bdb2f7a4713dcd294d
fanhaoyuan <418536538@qq.com>
2022-01-13 20:20:17 +0800

    types: 添加类型

---
+++ src/gitee-actions/constants/index.ts
@@ -0,0 +1,8 @@
+export const GITEE_ACTIONS_OPTIONS = 'gitee-actions-options';
+
+export const enum FolderType {
+    PULL_REQUEST = 'pull_request',
+    PUSH = 'push',
+    ISSUE = 'issue',
+    COMMENT = 'comment',
+}
