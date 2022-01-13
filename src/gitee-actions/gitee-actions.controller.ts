331ddd55280751025c1fe8357f7133b701772861
fanhaoyuan <418536538@qq.com>
2022-01-13 20:24:59 +0800

    feat: 添加触发流程的webhooks接口

---
+++ src/gitee-actions/gitee-actions.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { GiteePullRequestHooksDto } from './dto';
+import { PullRequestService } from './services';
+
+@Controller('gitee_actions')
+export class GiteeActionsController {
+    @Inject()
+    private readonly pullRequestService: PullRequestService;
+
+    @Post('pull_request')
+    async withPullRequest(@Body() giteePullRequestHooksDto: GiteePullRequestHooksDto) {
+        if (!this.pullRequestService.validActions(giteePullRequestHooksDto.action)) {
+            return {
+                code: 200,
+                message: '该钩子不需要触发CI',
+            };
+        }
+
+        const { action } = giteePullRequestHooksDto;
+
+        switch (action) {
+            case 'open':
+                this.pullRequestService.create(giteePullRequestHooksDto);
+                break;
+
+            case 'update':
+                this.pullRequestService.update(giteePullRequestHooksDto);
+                break;
+
+            case 'merge':
+            case 'close':
+                this.pullRequestService.remove(giteePullRequestHooksDto);
+                break;
+        }
+
+        return {
+            code: 200,
+            message: '触发CI成功',
+        };
+    }
+}
