861bf80330a4d484022c90340dd9fcba70075147
fanhaoyuan <418536538@qq.com>
2022-02-07 15:45:47 +0800

    fix: 修复删除操作也会触发工作流的问题

--- src/gitee-actions/modules/push/guards/validator.ts
+++ src/gitee-actions/modules/push/guards/validator.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, Injectable, NotAcceptableException } from '@nestjs/common';
 import { GiteePushHooksDTO } from '../dto';
 
 /**
@@ -9,7 +9,13 @@ export class ValidatorGuard implements CanActivate {
     canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
 
-        const { hook_name } = request.body as GiteePushHooksDTO;
+        const { hook_name, deleted } = request.body as GiteePushHooksDTO;
+
+        if (deleted) {
+            throw new NotAcceptableException({
+                message: '暂不支持删除操作触发工作流',
+            });
+        }
 
         const required = ['push_hooks', 'tag_push_hooks'];
 
