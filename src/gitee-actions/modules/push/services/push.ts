1d04ff25a908a9ae0991e94dc585ec3e7b80e355
fanhaoyuan <418536538@qq.com>
2022-02-07 15:06:29 +0800

    fix: 修复`push`钩子没有注入环境变量的问题

--- src/gitee-actions/modules/push/services/push.ts
+++ src/gitee-actions/modules/push/services/push.ts
@@ -19,6 +19,7 @@ export class PushService {
             remote: this.configService.getRemoteByURL('gitee', remote.http),
             branch,
             message,
+            inject: push,
         });
     }
 }
