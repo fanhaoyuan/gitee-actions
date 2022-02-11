f20fe0976d52a63811e5b7c47483ede32d609589
fanhaoyuan <418536538@qq.com>
2022-02-11 23:18:15 +0800

    feat: 添加 cli 路径解析功能

--- bin/app.ts
+++ bin/app.ts
@@ -1,8 +1,29 @@
 import { Command } from 'commander';
 import { version } from '../package.json';
 import { bootstrap } from '../src/bootstrap';
+import * as path from 'path';
+import * as fs from 'fs-extra';
 
 const program = new Command();
+
+/**
+ * 获取绝对路径
+ * @param p
+ * @returns
+ */
+function getAbsolutePath(p: string) {
+    if (path.isAbsolute(p)) {
+        return p;
+    }
+
+    const resolvedPath = path.resolve(process.cwd(), p);
+
+    if (!fs.pathExistsSync(resolvedPath)) {
+        return null;
+    }
+
+    return resolvedPath;
+}
 
 program
     .name('gitee-actions')
@@ -10,6 +31,25 @@ program
     .helpOption('-h, --help', '帮助信息')
     .version(version, '-v, --version', '版本信息')
     .option('-c, --config <path>', '配置项路径')
-    .action(options => bootstrap(options));
+    .option('-w, --workspace <path>', '工作区路径')
+    .action(options => {
+        const configPath = getAbsolutePath(options.config);
+
+        if (!configPath) {
+            throw new Error('配置项路径解析错误');
+        }
+
+        options.config = configPath;
+
+        const workspacePath = getAbsolutePath(options.workspace);
+
+        if (!workspacePath) {
+            throw new Error('工作区路径解析错误');
+        }
+
+        options.workspace = workspacePath;
+
+        bootstrap(options);
+    });
 
 program.parse(process.argv);
