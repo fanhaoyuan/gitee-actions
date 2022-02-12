a320ef1eaace4ec0d2a758b575102c9644eb9401
fanhaoyuan <418536538@qq.com>
2022-02-12 16:00:02 +0800

    chore: 补充提交

---
+++ src/helpers/define_config.ts
@@ -0,0 +1,10 @@
+import { GlobalConfig } from '@/modules';
+
+/**
+ * 定义配置项的辅助函数
+ * @param config
+ * @returns
+ */
+export function defineConfig(config: GlobalConfig): GlobalConfig {
+    return config;
+}
