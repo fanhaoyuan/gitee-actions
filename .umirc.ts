2994264c3b5f2dc5f7ead876aa94e9420a9a81de
fanhaoyuan <418536538@qq.com>
2022-02-16 00:08:00 +0800

    build: 添加文档配置项

---
+++ .umirc.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+    title: 'Gitee Actions',
+    mode: 'site',
+    // more config: https://d.umijs.org/config
+    description: 'Gitee Webhooks ✖️ Github Actions',
+    locales: [['zh-CN', '中文']],
+    logo: '/logo.svg',
+    favicon: '/logo.svg',
+    navs: [
+        {
+            title: '指南',
+            path: '/guide',
+        },
+        {
+            title: '配置项',
+            path: '/config',
+        },
+        {
+            title: 'API',
+            path: '/api',
+        },
+        {
+            title: '更新日志',
+            path: '/release',
+        },
+        {
+            title: 'GitHub',
+            path: 'https://github.com/fanhaoyuan/gitee-actions',
+        },
+    ],
+});
